cfbc28bec26f1d74ce6809d067c0df82464360d9
Jinzen Kuriakose <110563028+Jinzen001@users.noreply.github.com>
2025-08-12 21:40:27 +0530

    Add files via upload

---
+++ employeemanager2/src/com/litmus7/employeemanager/util/Response.java
@@ -0,0 +1,46 @@
+package com.litmus7.employeemanager.util;
+
+public class Response<T> {
+    private String status;   
+    private String message; 
+    private T data;          
+
+    public Response(String status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "status='" + status + '\'' +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
